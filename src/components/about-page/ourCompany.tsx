84162a127891cfa9cc589e08ef98217f66d80efc
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-06-02 01:02:22 +0600

    s

--- src/components/about-page/ourCompany.tsx
+++ src/components/about-page/ourCompany.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import TextBreak from '../ui/TextBreak';
 import TitleContainer from './TitleContainer';
-import Container from '../home-page/Container';
+import { Container, Grid } from '@/components';
 import Subtitle from '../home-page/Subtitle';
 
 interface OurCompanySection {
@@ -22,54 +20,54 @@ interface HomeData {
 	};
 }
 
-const OurCompany = () => {
-	const [ourCompany, setOurCompany] = useState<OurCompanySection | null>(null);
+const images = [
+	{
+		url: '/shams.jpg',
+		alt: 'Construction Site',
+	},
+	{
+		url: '/DSC00128.JPG',
+		alt: 'Sinamm Engineering Office',
+	},
+	{
+		url: '/now.JPG',
+		alt: 'Engineering Design',
+	},
+];
 
-	useEffect(() => {
-		fetch('/homeData.json')
-			.then(res => res.json())
-			.then((data: HomeData) => {
-				setOurCompany(data?.about?.ourCompanySection || null);
-			})
-			.catch(error => {
-				console.error('Error loading about data:', error);
-			});
-	}, []);
+const OurCompany = () => {
 	return (
 		<Container className=''>
-			<div className='grid grid-cols-1 lg:grid-cols-2 gap-16 items-center'>
-				<div className='space-y-6 animate-slide-in-left'>
-					{/* <h2 className='text-3xl md:text-8xl font-primary font-bold text-mainText'></h2> */}
-					<TitleContainer>{ourCompany?.title}</TitleContainer>
-					<Subtitle className='md:w-3/4'>{ourCompany?.paragraphs}</Subtitle>
+			<Grid className='grid-cols-1 lg:grid-cols-2 gap-16 items-center'>
+				<div className='space-y-6'>
+					<TitleContainer>Our Company</TitleContainer>
+					<Subtitle className='md:w-3/4'>{`SINAMM ENGINEERING LIMITED, named as SINAMM, was conceived and formed in the year 2004 by a team of qualified Engineers and other professionals possessing long-standing experience in various fields of constructional activities with a vision and mission to play a significant role in the development and construction sector in the near future.`}</Subtitle>
 				</div>
-				<div className='grid grid-cols-2 gap-[2px] animate-slide-in-right'>
-					<>
-						<div className='overflow-hidden bg-[#0d0d0'>
-							<img
-								src={ourCompany?.images[0]?.url}
-								alt={ourCompany?.images[0]?.alt || 'Sinamm Engineering Office'}
-								className='w-full h-full object-cover'
-							/>
-						</div>
-						<div className='overflow-hidden row-span-2 bg-[#0d0d0'>
-							<img
-								src={ourCompany?.images[1]?.url}
-								alt={ourCompany?.images[1]?.alt}
-								className='w-full h-full object-cover'
-							/>
-						</div>
+				<Grid className='grid-cols-2 gap-[2px]'>
+					<div className='overflow-hidden bg-[#0d0d0]'>
+						<img
+							src={images[0]?.url}
+							alt={images[0]?.alt || 'Sinamm Engineering Office'}
+							className='w-full h-full object-cover'
+						/>
+					</div>
+					<div className='overflow-hidden row-span-2 bg-[#0d0d0]'>
+						<img
+							src={images[1]?.url}
+							alt={images[1]?.alt}
+							className='w-full h-full object-cover'
+						/>
+					</div>
 
-						<div className='overflow-hidden bg-[#0d0d0d]'>
-							<img
-								src={ourCompany?.images[2]?.url}
-								alt={ourCompany?.images[2]?.alt}
-								className='w-full h-full object-cover'
-							/>
-						</div>
-					</>
-				</div>
-			</div>
+					<div className='overflow-hidden bg-[#0d0d0d]'>
+						<img
+							src={images[2]?.url}
+							alt={images[2]?.alt}
+							className='w-full h-full object-cover'
+						/>
+					</div>
+				</Grid>
+			</Grid>
 		</Container>
 	);
 };
